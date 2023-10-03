5901da10c9a4eb1a55802d7ba606845e9eda7af8
faouziaen <faouziaennaama@gmail.com>
2023-10-03 12:18:34 +0100

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,6 +8,7 @@ def uppercase(str):
             c = chr(ord(c) - 32)
             print("{}".format(c), end="")
             print("")
+
 if __name__ == "__main__":
     uppercase("best")
     uppercase("Best School 98 Battery street")
