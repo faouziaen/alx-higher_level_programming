0f403724fd4bec1f3e391858b6832d6610b054e6
faouziaen <faouziaennaama@gmail.com>
2023-10-04 00:38:56 +0100

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,9 @@
+
 #!/usr/bin/python3
-# 8-uppercase.py
-
-
 def uppercase(str):
+    result = ""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-            print("")
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
+        result += c
+    print("{}".format(result))
