b8b00007d01d0e1a4f3432eec704701d39822c38
faouziaen <faouziaennaama@gmail.com>
2023-10-04 00:03:08 +0100

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z') + 1):
-        print(chr(letter), end='')
-
-        print()
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
