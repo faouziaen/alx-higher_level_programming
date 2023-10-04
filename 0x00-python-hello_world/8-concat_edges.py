3bcb7b4fcb6451b0e22e750828c9df491b4ec303
faouziaen <faouziaennaama@gmail.com>
2023-10-04 01:09:10 +0100

    update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming" \
               " language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented"):str.find("object-oriented") + 20] + " with Python")
+print(str[str.find("object-oriented"):str.find("object-oriented") + 20] + " with Python", end="\n\n")
