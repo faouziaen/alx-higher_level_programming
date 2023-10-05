300567ebbdcf95fa26ffb809b09778e3dc8c4b45
faouziaen <faouziaennaama@gmail.com>
2023-10-05 13:30:44 +0100

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from add_0 import add
+
+# Assigning values to variables
+a = 1
+b = 2
+
+# Calling the add function and storing the result
+result = add(a, b)
+
+# Printing the result with a formatted string
+print("{} + {} = {}".format(a, b, result))
