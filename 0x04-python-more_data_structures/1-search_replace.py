77f813130880d350d491c339f4326bb191eceb35
faouziaen <faouziaennaama@gmail.com>
2023-10-11 12:06:02 +0100

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    # Create a new list by iterating through the original list
+    new_list = [replace if element == search else element for element in my_list]
+    return new_list
