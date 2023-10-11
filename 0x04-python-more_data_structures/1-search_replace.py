5a0e5d270014007860678108b05ad00c0489ab99
faouziaen <faouziaennaama@gmail.com>
2023-10-11 12:07:30 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    # Create a new list by iterating through the original list
-    new_list = [replace if element == search else element for element in my_list]
-    return new_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
