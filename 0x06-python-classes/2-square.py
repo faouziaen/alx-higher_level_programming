dd4d727035434d3e0e36923aa2237f51b2716727
faouziaen <faouziaennaama@gmail.com>
2023-10-24 14:51:20 +0100

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """
+    This is the Square class.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square (optional).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
