d201756198f7a9aa6fd087d32184dc718056d83c
faouziaen <faouziaennaama@gmail.com>
2023-10-30 15:41:58 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize the rectangle."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -48,3 +48,16 @@ class Rectangle:
             return 0
         else:
             return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Print the rectangle with # character."""
+        rect_str = ''
+        if self.__width == 0 or self.__height == 0:
+            return rect_str
+        for i in range(self.__height):
+            rect_str += '#' * self.__width + '\n'
+        return rect_str[:-1]  # Remove the last newline character
+
+    def __repr__(self):
+        """Return a string representation of the rectangle."""
+        return 'Rectangle(' + str(self.__width) + ', ' + str(self.__height) + ')'
