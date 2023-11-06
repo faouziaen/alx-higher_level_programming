ffcf0023aef6d2fa0d3eddad108b858fff5b53d7
faouziaen <faouziaennaama@gmail.com>
2023-11-06 12:51:11 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """
+    A class named BaseGeometry.
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an Exception if called.
+        """
+        raise Exception("area() is not implemented")
